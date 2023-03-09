8092f7c9a869e02f30cea78a8f1801f9e93e3d55
GameExplorer <73129125+GameExplorer@users.noreply.github.com>
2023-03-09 09:04:22 +0100

    .

--- src/predavanje04/veckratniki.java
+++ src/predavanje04/veckratniki.java
@@ -1,9 +1,11 @@
 package predavanje04;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class veckratniki {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.print("Vpiši n: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -14,9 +16,14 @@ public class veckratniki {
         System.out.print("Vpiši b: ");
         int b = sc.nextInt();
 
+        sc.close();
+        PrintWriter pw = new PrintWriter("veckratniki.tx");
+
         for(int i = a; i <= b; i++) {
-            System.out.printf("%d * %d = %d", i,n, i * n);
+            pw.printf("%d * %d = %d", i,n, i * n);
         }
+
+        pw.close();
     }
 }
 
