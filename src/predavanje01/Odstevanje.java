5a57e8bb8e1de561c67db024e7bb14752bb368e9
GameExplorer <petar.petrovic2001@gmail.com>
2023-07-20 10:10:00 +0200

    P1

--- src/predavanje01/Odstevanje.java
+++ src/predavanje01/Odstevanje.java
@@ -1,5 +1,8 @@
 package predavanje01;
 
+/**
+ * Odštevanje
+ */
 public class Odstevanje {
     public static void main(String[] args) {
         for(int i = 10; i > 0; i--) System.out.printf("%d%n", i);
